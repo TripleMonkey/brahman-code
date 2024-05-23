16f208c0f575ea32eec977a6b676e101826f7bc1
TripleMonkey <triplemonkeystudio@gmail.com>
2024-05-22 23:09:16 -0400

    MOD: create image classifier and picker

--- BrahmanCode/ViewModels/ImageClassifier.swift
+++ BrahmanCode/ViewModels/ImageClassifier.swift
@@ -5,4 +5,21 @@
 //  Created by Nigel Krajewski on 5/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+
+class ImageClassifier: ObservableObject {
+
+    @Published private var classifier = Classifier()
+
+    var imageClass: String? {
+        classifier.results
+    }
+
+    // MARK: Intent(s)
+    func detect(uiImage: UIImage) {
+        guard let ciImage = CIImage (image: uiImage) else { return }
+        classifier.detect(ciImage: ciImage)
+    }
+}
+
