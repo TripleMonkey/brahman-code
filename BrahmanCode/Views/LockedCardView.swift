ad3e4fea70572658a8cc18cb4533e6993543778a
TripleMonkey <triplemonkeystudio@gmail.com>
2024-06-12 13:30:55 -0400

    ADD: Add locked and unlocked card views

---
+++ BrahmanCode/Views/LockedCardView.swift
@@ -0,0 +1,18 @@
+//
+//  LockedCardView.swift
+//  FloraDex
+//
+//  Created by Nigel Krajewski on 6/12/24.
+//
+
+import SwiftUI
+
+struct LockedCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LockedCardView()
+}
---
+++ BrahmanCode/Views/UnlockedCardView.swift
@@ -0,0 +1,18 @@
+//
+//  UnlockedCardView.swift
+//  FloraDex
+//
+//  Created by Nigel Krajewski on 6/12/24.
+//
+
+import SwiftUI
+
+struct UnlockedCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnlockedCardView()
+}
